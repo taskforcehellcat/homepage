f2292a886521fd724a9c07aeb38cd3cefce606e1
Emmi <30769742+pantoffelemmy@users.noreply.github.com>
2023-08-04 03:12:41 +0200

    very rough fetching logic

---
+++ src/routes/aktuelles/+page.svelte
@@ -0,0 +1,9 @@
+
+<script lang='ts'>
+    import type { Campaign } from './.d.ts';
+
+    export let data;
+    const campaignData: Campaign = data.campaignData;
+</script>
+
+<p>{campaignData.title}</p>
